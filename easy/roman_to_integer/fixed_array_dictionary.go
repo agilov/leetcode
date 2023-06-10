e203a169c3a6091ebb334a85f64f52f9735fd30d
agilov <agilovr@gmail.com>
2023-06-10 20:38:51 +0300

    fixed array roman to integer moved to package-level declaration

--- easy/roman_to_integer/fixed_array_dictionary.go
+++ easy/roman_to_integer/fixed_array_dictionary.go
@@ -1,14 +1,15 @@
 package roman_to_integer
 
+var roman = [256]int{'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
 func RomanToIntFixedArray(s string) int {
-	var roman = [256]int{'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
 	var result, previous, current int
 
 	for i := len(s) - 1; i >= 0; i-- {
 		current = roman[s[i]]
 
-		if current < previous {
+		if roman[s[i]] < previous {
 			result -= current
 		} else {
 			result += current
