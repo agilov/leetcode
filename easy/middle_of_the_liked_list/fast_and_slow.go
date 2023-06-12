ed0c5ef69f3f5e266499af0e00e849469874ec0a
agilov <agilovr@gmail.com>
2023-06-12 23:17:01 +0300

    middle ofr tthee linked list fast and slow

---
+++ easy/middle_of_the_liked_list/fast_and_slow.go
@@ -0,0 +1,12 @@
+package middle_of_the_liked_list
+
+func middleNodeFastAndSlow(head *ListNode) *ListNode {
+	slow, fast := head, head
+
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	return slow
+}
--- easy/middle_of_the_liked_list/loop_test.go
+++ easy/middle_of_the_liked_list/loop_test.go
@@ -2,9 +2,25 @@ package middle_of_the_liked_list
 
 import "testing"
 
-func TestFizzBuzzNaive(t *testing.T) {
+func TestMiddleLinkedListNaive(t *testing.T) {
 	list := &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, nil}}}}
 	result := middleNode(list)
+
+	if result.Val != 3 {
+		t.Error("Shoul be 3")
+	}
+
+	list = &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{5, nil}}}}}
+	result = middleNode(list)
+
+	if result.Val != 3 {
+		t.Errorf("Should be 3 got %v", result.Val)
+	}
+}
+
+func TestMiddleLinkedListFastAndSlow(t *testing.T) {
+	list := &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, nil}}}}
+	result := middleNodeFastAndSlow(list)
 
 	if result.Val != 3 {
 		t.Error("Shoul be 3")
