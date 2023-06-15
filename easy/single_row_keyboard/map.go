e8ab3120e733e3e6cddb82ce04e5da59f3b911a3
agilov <agilovr@gmail.com>
2023-06-15 22:48:18 +0300

    single row keyboard

---
+++ easy/single_row_keyboard/map.go
@@ -0,0 +1,27 @@
+package single_row_keyboard
+
+func calculateTime(keyboard string, word string) int {
+	mapping := [26]int{}
+
+	for i, c := range []byte(keyboard) {
+		mapping[c-'a'] = i
+	}
+
+	result := 0
+	cur := 0
+
+	for _, c := range []byte(word) {
+		idx := mapping[c-'a']
+		result += abs(idx - cur)
+		cur = idx
+	}
+
+	return result
+}
+
+func abs(n int) int {
+	if n < 0 {
+		return -n
+	}
+	return n
+}
